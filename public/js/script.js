095571c0069d8bcd410d9a79b9c393382c080809
Jose Salazar <jose.salazar.home@gmail.com>
2017-08-07 18:08:24 -0700

    Delete Functionality

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
 	var Spotify = require('node-spotify-api');
 
 	$(document).on("submit", "#album-search", addAlbums);
+	$(document).on("click", "button.delete", deleteAlbum);
 
 	var albums = [];
 
-	function spotifySearch(album) {
+	function spotifySearch(album, id) {
 		var spotify = new Spotify({
 		  id: "35563073063e418aa3673cc30523cc50",
 		  secret: "f568229cd07c483d99097e09d419310e"
@@ -25,6 +26,7 @@ $(document).ready(function() {
 						<div>${data.tracks.items[0].artists[0].name}</div>
 						<div>${data.tracks.items[0].album.name}</div>
 						<div><a href=${data.tracks.items[0].album.external_urls.spotify} target="_blank">Play it Here!</a></div>
+						<button class="delete btn-danger" data-id=${id}>Remove</button>
 					</div>
 					<hr>`
 				);
@@ -47,8 +49,17 @@ $(document).ready(function() {
 		albumInfo.empty();
 		for (var i = 0; i < albums.length; i++) {
 			var albumQuery = `${albums[i].title} - ${albums[i].artist}`;
-			spotifySearch(albumQuery);
+			spotifySearch(albumQuery, albums[i].id);
 		}
+	}
+
+	function deleteAlbum(event) {
+		event.stopPropagation();
+		var id = $(this).attr("data-id");
+		$.ajax({
+			method: "DELETE",
+			url: "/api/albums/" + id
+		}).done(getAlbumInfo);
 	}
 
 	// Add Album Info
