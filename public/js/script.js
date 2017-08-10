94406206c225cf69ab31f0aa8f54a899115a06c8
Jose Salazar <jose.salazar.home@gmail.com>
2017-08-09 17:05:02 -0700

    Final Changes

--- public/js/script.js
+++ public/js/script.js
@@ -2,42 +2,11 @@ $(document).ready(function() {
 	var newTerm = $("input.search-term");
 	var newArtist = $("input.search-artist");
 	var albumInfo = $("#album-info");
-	var Spotify = require('node-spotify-api');
 
 	$(document).on("submit", "#album-search", addAlbums);
 	$(document).on("click", "button.delete", deleteAlbum);
 
 	var albums = [];
-
-	function spotifySearch(album, id) {
-		var spotify = new Spotify({
-		  id: "58e29f27ad1449f3a22a6485f5be2b6c",
-		  secret: "4008a3414e834ec48495c1186c149c6e"
-		});
-
-		spotify.search({ type: 'track', query: album }, function(err, data) {
-			if(data == null) {
-			  	console.log("Didn't work mate...");
-			} else {
-				var rowsAdded = [];
-				rowsAdded.push(
-					`<div class="artist-block">
-						<img src=${data.tracks.items[0].album.images[2].url}>
-						<div>${data.tracks.items[0].artists[0].name}</div>
-						<div>${data.tracks.items[0].album.name}</div>
-						<div><a href=${data.tracks.items[0].album.external_urls.spotify} target="_blank">Play it Here!</a></div>
-						<audio src=${data.tracks.items[0].preview_url} controls>
-						</audio>
-						<button class="delete btn-danger" data-id=${id}>Remove</button>
-					</div>
-					<hr>`
-				);
-				albumInfo.append(rowsAdded);
-				console.log(data);
-				console.log(data.tracks.items[2].preview_url);
-			}
-		});
-	}
 
 	// Get Album Info
 	function getAlbumInfo() {
@@ -52,7 +21,7 @@ $(document).ready(function() {
 		albumInfo.empty();
 		for (var i = 0; i < albums.length; i++) {
 			var albumQuery = `${albums[i].title} - ${albums[i].artist}`;
-			spotifySearch(albumQuery, albums[i].id);
+			console.log(albumQuery);
 		}
 	}
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,10 @@
 var db = require("../models");
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+	id: "58e29f27ad1449f3a22a6485f5be2b6c",
+	secret: "4008a3414e834ec48495c1186c149c6e"
+})
 
 module.exports = function(app) {
 	app.get("/api/albums", function(req, res) {
@@ -8,12 +14,16 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/albums", function(req, res) {
-		db.Album.create({
-			title: req.body.title,
-			artist: req.body.artist
-		}).then(function(dbAlbum) {
-			res.json(dbAlbum);
+		spotify.search({ type: 'track', query: req.body.title + " " +req.body.artist }).then(function(passOn){
+			res.json(passOn);
+			console.log(passOn.tracks.items[0].preview_url);
 		});
+		// db.Album.create({
+		// 	title: req.body.title,
+		// 	artist: req.body.artist
+		// }).then(function(dbAlbum) {
+		// 	res.json(dbAlbum);
+		// });
 	});
 
 	app.delete("/api/albums/:id", function(req, res) {
